wilsonforjobs/npm

publish: Improve the use of progress bars w/ publish

lib/publish.js
@@ -33,10 +33,12 @@ function publish (args, isRetry, cb) {
   if (args.length === 0) args = ['.']
   if (args.length !== 1) return cb(publish.usage)
 
+  var progress = log.newItem('publish', 6, 6)
   log.verbose('publish', args)
 
   var t = npm.config.get('tag').trim()
   if (semver.validRange(t)) {
+    progress.finish()
     var er = new Error('Tag name must not be a valid SemVer range: ' + t)
     return cb(er)
   }
@@ -45,6 +47,7 @@ function publish (args, isRetry, cb) {
   // if it's a local folder, then run the prepublish there, first.
   readJson(path.resolve(arg, 'package.json'), function (er, data) {
     if (er && er.code !== 'ENOENT' && er.code !== 'ENOTDIR') return cb(er)
+    progress.completeWork(1)
 
     if (data) {
       if (!data.name) return cb(new Error('No name provided'))
@@ -54,8 +57,8 @@ function publish (args, isRetry, cb) {
     // Error is OK. Could be publishing a URL or tarball, however, that means
     // that we will not have automatically run the prepublish script, since
     // that gets run when adding a folder to the cache.
-    if (er) return cacheAddPublish(arg, false, isRetry, cb)
-    else cacheAddPublish(arg, true, isRetry, cb)
+    if (er) return cacheAddPublish(arg, false, isRetry, progress, cb)
+    else cacheAddPublish(arg, true, isRetry, progress, cb)
   })
 }
 
@@ -64,25 +67,35 @@ function publish (args, isRetry, cb) {
 // like a tarball or name@version thing.
 // That means that we can run publish/postpublish in the dir, rather than
 // in the cache dir.
-function cacheAddPublish (dir, didPre, isRetry, cb) {
+function cacheAddPublish (dir, didPre, isRetry, progress, cb) {
   npm.commands.cache.add(dir, null, null, false, function (er, data) {
     if (er) return cb(er)
+    progress.completeWork(1)
+
+    function completedOne (cb) {
+      progress.completeWork(1)
+      cb()
+    }
     log.silly('publish', data)
     var cachedir = path.resolve(cachedPackageRoot(data), 'package')
     chain(
       [
-        !didPre && [lifecycle, data, 'prepublish', cachedir],
-        [publish_, dir, data, isRetry, cachedir],
-        [lifecycle, data, 'publish', didPre ? dir : cachedir],
-        [lifecycle, data, 'postpublish', didPre ? dir : cachedir]
+        !didPre && [lifecycle, data, 'prepublish', cachedir], [completedOne],
+        [publish_, dir, data, isRetry, cachedir, log.newItem('publish', 1)],
+        [lifecycle, data, 'publish', didPre ? dir : cachedir], [completedOne],
+        [lifecycle, data, 'postpublish', didPre ? dir : cachedir], [completedOne]
       ],
-      cb
+      function () {
+        cb.apply(null, arguments)
+      }
     )
   })
 }
 
-function publish_ (arg, data, isRetry, cachedir, cb) {
+function publish_ (arg, data, isRetry, cachedir, progress, cb) {
   if (!data) return cb(new Error('no package.json file found'))
+
+  progress.addWork(1)
 
   var mappedConfig = getPublishConfig(
     data.publishConfig,
@@ -105,6 +118,8 @@ function publish_ (arg, data, isRetry, cachedir, cb) {
 
   mapToRegistry(data.name, config, function (er, registryURI, auth, registryBase) {
     if (er) return cb(er)
+
+    progress.completeWork(1)
 
     var tarballPath = cachedir + '.tgz'
 
@@ -135,6 +150,7 @@ function publish_ (arg, data, isRetry, cachedir, cb) {
 
     log.showProgress('publish:' + data._id)
     registry.publish(registryBase, params, function (er) {
+      progress.completeWork(1)
       if (er && er.code === 'EPUBLISHCONFLICT' &&
           npm.config.get('force') && !isRetry) {
         log.warn('publish', 'Forced publish over ' + data._id)

lochez/ez

lib/publish.js
@@ -34,10 +34,12 @@ function publish (args, isRetry, cb) {
   if (args.length === 0) args = ['.']
   if (args.length !== 1) return cb(publish.usage)
 
+  var progress = log.newItem('publish', 6, 6)
   log.verbose('publish', args)
 
   var t = npm.config.get('tag').trim()
   if (semver.validRange(t)) {
+    progress.finish()
     var er = new Error('Tag name must not be a valid SemVer range: ' + t)
     return cb(er)
   }
@@ -46,6 +48,7 @@ function publish (args, isRetry, cb) {
   // if it's a local folder, then run the prepublish there, first.
   readJson(path.resolve(arg, 'package.json'), function (er, data) {
     if (er && er.code !== 'ENOENT' && er.code !== 'ENOTDIR') return cb(er)
+    progress.completeWork(1)
 
     if (data) {
       if (!data.name) return cb(new Error('No name provided'))
@@ -56,6 +59,7 @@ function publish (args, isRetry, cb) {
     if (er) {
       npm.commands.cache.add(arg, null, null, false, function (er, data) {
         if (er) return cb(er)
+        progress.completeWork(1)
         log.silly('publish', data)
         var cached = path.resolve(cachedPackageRoot(data), 'package') + '.tgz'
         // *publish* lifecycle scripts aren't run when publishing a built artifact
@@ -66,15 +70,20 @@ function publish (args, isRetry, cb) {
       var dir = arg
       npm.commands.cache.add(dir, null, null, false, function (er, data) {
         if (er) return cb(er)
+        progress.completeWork(1)
+        function completedOne (cb) {
+          progress.completeWork(1)
+          cb()
+        }
         log.silly('publish', data)
         var cached = path.resolve(cachedPackageRoot(data), 'package') + '.tgz'
         // `prepublish` and `prepare` are run by cache.add
         chain(
           [
-            [lifecycle, data, 'prepublishOnly', dir],
+            [lifecycle, data, 'prepublishOnly', dir], [completedOne]
             [publish_, dir, data, isRetry, cached],
-            [lifecycle, data, 'publish', dir],
-            [lifecycle, data, 'postpublish', dir]
+            [lifecycle, data, 'publish', dir], [completedOne]
+            [lifecycle, data, 'postpublish', dir], [completedOne]
           ],
           cb
         )
@@ -83,8 +92,10 @@ function publish (args, isRetry, cb) {
   })
 }
 
-function publish_ (arg, data, isRetry, cached, cb) {
+function publish_ (arg, data, isRetry, cached, progress, cb) {
   if (!data) return cb(new Error('no package.json file found'))
+
+  progress.addWork(1)
 
   var mappedConfig = getPublishConfig(
     data.publishConfig,
@@ -107,6 +118,8 @@ function publish_ (arg, data, isRetry, cached, cb) {
 
   mapToRegistry(data.name, config, function (er, registryURI, auth, registryBase) {
     if (er) return cb(er)
+
+    progress.completeWork(1)
 
     // we just want the base registry URL in this case
     log.verbose('publish', 'registryBase', registryBase)
@@ -135,6 +148,7 @@ function publish_ (arg, data, isRetry, cached, cb) {
 
     log.showProgress('publish:' + data._id)
     registry.publish(registryBase, params, function (er) {
+      progress.completeWork(1)
       if (er && er.code === 'EPUBLISHCONFLICT' &&
           npm.config.get('force') && !isRetry) {
         log.warn('publish', 'Forced publish over ' + data._id)